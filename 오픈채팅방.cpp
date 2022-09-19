ad7b6f54a2b73807a15e349c8edab318299d1090
park-yina <111878820+park-yina@users.noreply.github.com>
2022-09-19 17:46:23 +0900

    Create 오픈채팅방.cpp
    https://school.programmers.co.kr/learn/courses/30/lessons/42888

---
+++ 오픈채팅방.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <map>
+#include<sstream>
+using namespace std;
+map<string,string>user_nick;
+vector<pair<string,string>>result;
+vector<string> solution(vector<string> record) {
+    vector<string> answer;
+    for(int i=0;i<record.size();i++){
+        string input,id,nick_name;
+        stringstream sstream;
+        sstream.str(record[i]);
+        sstream>>input>>id>>nick_name;
+        if(input=="Enter"){
+            user_nick[id]=nick_name;
+            result.push_back(make_pair(id,"들어왔습니다."));
+        }
+        else if(input=="Leave")
+            result.push_back(make_pair(id,"나갔습니다."));
+       else if(input=="Change")
+           user_nick[id]=nick_name;
+    }
+        for(int i=0;i<result.size();i++){
+            answer.push_back(user_nick[result[i].first]+"님이 "+result[i].second);
+        }
+    return answer;
+}
